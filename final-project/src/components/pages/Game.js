c2d173a5374998c4938fa1fd373123faf31f9d83
Rydralain <37129740+Rydralain@users.noreply.github.com>
2018-10-02 17:43:54 -0700

    Added URL param for game ID in react routing

--- final-project/src/components/pages/Game.js
+++ final-project/src/components/pages/Game.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Board from "../Board/Board";
 
-const Game = () => (
+const Game = ({match}) => (
     <div>
       <h1>Game</h1>
-
+      Game ID: {match.params.gameID}
+      <Board />
     </div>
   );
 
