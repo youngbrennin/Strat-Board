7448b184630eca88d4c79221e8bc86e7997bfd94
mansouri <mansouri@email.arizona.edu>
2018-10-09 12:07:09 -0700

    added google button to splash page still not working though

--- final-project/src/components/pages/Splash/Splash.js
+++ final-project/src/components/pages/Splash/Splash.js
@@ -2,17 +2,26 @@ import React from "react";
 import "./Splash.css";
 
 const Splash = () => (
-    <div>
+  <div>
+    
       <div className="bg">
-      <div className="login">Welcome to Card Chess!
-      <br/>
-     
-      <button className="loginBtn">Login</button>
-      </div>
-      
+        <div className="login">Welcome to Card Chess!
+
+          <div className="jumbotron text-center">
+            <p>Welcome to Card Chess!</p>
+            <p>Login or Register with:</p>
+
+            
+            <a href="/auth/google" class="btn btn-danger"><span class="fa fa-google-plus"></span> Google</a>
+
+          </div>
+          {/* <button className="loginBtn">Login</button> */}
+        </div>
+
       </div>
     </div>
 
-  );
+
+);
 
 export default Splash;
