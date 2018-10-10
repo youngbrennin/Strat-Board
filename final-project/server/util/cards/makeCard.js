147b4671b0f80f16d8b690e911676a85a23fb6a1
Rydralain <37129740+Rydralain@users.noreply.github.com>
2018-10-09 20:51:42 -0700

    Added utilities for card paths

---
+++ final-project/server/util/cards/makeCard.js
@@ -0,0 +1,139 @@
+// makeCard factory returns an object for the specified card
+
+const blankCard = function() {
+    
+
+    
+
+
+}
+
+const makePath = require('./makePath');
+
+const makeCard = function(cardName) {
+    this.paths = [];
+
+    this.addPath = function(pathArray){
+        // array should contain a series of 2-variable arrays for relative x,y positions
+        this.paths.push(pathArray);
+    }
+
+    switch(cardName){
+        case "rook":
+            makePath.addSegment("standard", 0, 1, 4);
+            this.addPath(makePath.getAndReset());
+            makePath.addSegment("standard", 2, 1, 4);
+            this.addPath(makePath.getAndReset());
+            makePath.addSegment("standard", 4, 1, 4);
+            this.addPath(makePath.getAndReset());
+            makePath.addSegment("standard", 6, 1, 4);
+            this.addPath(makePath.getAndReset());
+            break;
+        case "pawn":
+            makePath.addSegment("move", 0, 1, 1);
+            this.addPath(makePath.getAndReset());
+            makePath.addSegment("attack", 1, 1, 1);
+            this.addPath(makePath.getAndReset());
+            makePath.addSegment("attack", 7, 1, 1);
+            this.addPath(makePath.getAndReset());
+            break;
+        case "knight":
+            this.addPath([{
+                type: "standard",
+                x: -1,
+                y: -2
+            }]);
+            this.addPath([{
+                type: "standard",
+                x: -2,
+                y: -1
+            }]);
+            this.addPath([{
+                type: "standard",
+                x: 1,
+                y: -2
+            }]);
+            this.addPath([{
+                type: "standard",
+                x: 2,
+                y: -1
+            }]);
+            this.addPath([{
+                type: "standard",
+                x: 2,
+                y: 1
+            }]);
+            this.addPath([{
+                type: "standard",
+                x: 1,
+                y: 2
+            }]);
+            this.addPath([{
+                type: "standard",
+                x: -1,
+                y: 2
+            }]);
+            this.addPath([{
+                type: "standard",
+                x: -2,
+                y: 1
+            }]);
+            break;
+        case "bishop":
+            makePath.addSegment("standard", 1, 1, 4);
+            this.addPath(makePath.getAndReset());
+            makePath.addSegment("standard", 3, 1, 4);
+            this.addPath(makePath.getAndReset());
+            makePath.addSegment("standard", 5, 1, 4);
+            this.addPath(makePath.getAndReset());
+            makePath.addSegment("standard", 7, 1, 4);
+            this.addPath(makePath.getAndReset());
+            break;
+        case "king":
+            makePath.addSegment("standard", 0, 1, 1);
+            this.addPath(makePath.getAndReset());
+            makePath.addSegment("standard", 2, 1, 1);
+            this.addPath(makePath.getAndReset());
+            makePath.addSegment("standard", 4, 1, 1);
+            this.addPath(makePath.getAndReset());
+            makePath.addSegment("standard", 6, 1, 1);
+            this.addPath(makePath.getAndReset());
+            break;
+        case "queen":
+            makePath.addSegment("standard", 1, 1, 4);
+            this.addPath(makePath.getAndReset());
+            makePath.addSegment("standard", 3, 1, 4);
+            this.addPath(makePath.getAndReset());
+            makePath.addSegment("standard", 5, 1, 4);
+            this.addPath(makePath.getAndReset());
+            makePath.addSegment("standard", 7, 1, 4);
+            this.addPath(makePath.getAndReset());
+            makePath.addSegment("standard", 0, 1, 4);
+            this.addPath(makePath.getAndReset());
+            makePath.addSegment("standard", 2, 1, 4);
+            this.addPath(makePath.getAndReset());
+            makePath.addSegment("standard", 4, 1, 4);
+            this.addPath(makePath.getAndReset());
+            makePath.addSegment("standard", 6, 1, 4);
+            this.addPath(makePath.getAndReset());
+            break;
+    }
+
+    
+}
+
+module.exports = makeCard;
+
+
+// expected array format for paths
+// [
+//     {
+//         type: "move",
+//         x: 1,
+//         y: 0
+//     },
+//     {
+//         type: "attack",
+//         x: -1
+//     }
+// ]
