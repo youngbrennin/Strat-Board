2b96fe799158d06b5fe250ea815a48cca3459562
Michelangelo <youngbrennin20@gmail.com>
2018-10-11 19:20:10 -0700

    just adding stuff nothing too useful yet

--- final-project/src/components/pages/MatchMaking/MatchMaking.js
+++ final-project/src/components/pages/MatchMaking/MatchMaking.js
@@ -1,10 +1,19 @@
 import React from "react";
+import "./MatchMaking.css";
+
 
 const MatchMaking = () => (
-    <div>
-      <h1>Matchmaking</h1>
-
-    </div>
+  <div class="row">
+  <div class="col1 s12 m5">
+      <div class="card-panel">
+          <p className="leftHead">
+              Welcome back {}!
+              <span className="rightHead">Wins:{} Losses{}</span>
+          </p>
+      </div>
+  </div>
+</div>
   );
+
 
 export default MatchMaking;
