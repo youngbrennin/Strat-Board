7ddab5cd9336fbe5c804eaeebc2f28276d32a0a5
Rydralain <37129740+Rydralain@users.noreply.github.com>
2018-10-15 00:23:36 -0700

    added seed data for testing matchmaking page

---
+++ final-project/db/gameSeed.sql
@@ -0,0 +1,3 @@
+insert into games (id, player1, player1Name, active, createdAt, updatedAt) values (0, 1, "Bob", true, now(), now());
+insert into games (id, player1, player1Name, active, createdAt, updatedAt) values (1, 1, "Fred", false, now(), now());
+insert into games (id, player1, player1Name, player2, player2Name, active, createdAt, updatedAt) values (2, 3, "George", 4, "Sam", true, now(), now());
